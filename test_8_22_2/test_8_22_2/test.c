f184969f64e12526c4bb63438d4e3174d739d2a4
AmazGadZ <1127569558@qq.com>
2022-08-28 15:44:12 +0800

    2022.8.22

---
+++ test_8_22_2/test_8_22_2/test.c
@@ -0,0 +1,364 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		if (n % 2 == 1)
+//		{
+//			count++;
+//		}
+//		n = n / 2;
+//	}
+//	return count;
+//}
+
+//int count_bit_one(int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((n >> i) & 1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+
+//int count_bit_one(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+////n = n&(n-1)
+////13
+////1101 n
+////1100 n-1
+////1100 n
+////1011 n-1
+////1000 n
+////0111 n-1
+////0000 n
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	//写一个函数求a的二进制（补码）表示中有几个1
+//	int count = count_bit_one(a);//-1
+//	//-1
+//	//10000000000000000000000000000001
+//	//11111111111111111111111111111110
+//	//11111111111111111111111111111111
+//	//00000000000000000000000000000001
+//	//
+//
+//	//13
+//	//00000000000000000000000000001101
+//	printf("count = %d\n", count);
+//	//system("pause");//system库函数 - 执行系统命令 - pause(暂停)
+//	return 0;
+//}
+
+//int get_diff_bit(int m, int n)
+//{
+//	int tmp = m ^ n;
+//	int count = 0;
+//	//return count_bit_one(tmp);
+//	while (tmp)
+//	{
+//		tmp = tmp & (tmp - 1);
+//		count++;
+//	}
+//	return count;
+//
+//}
+//
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);
+//	int count = get_diff_bit(m, n);
+//	printf("count = %d\n", count);
+//	return 0;
+//}
+
+//void print(int m)
+//{
+//	int i = 0;
+//	printf("奇数位：\n");
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d ", (m >> i) & 1);
+//	}
+//	printf("\n");
+//	printf("偶数位：\n");
+//	for (i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d ", (m >> i) & 1);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	print(m);
+//	return 0;
+//}
+
+//void print(int* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	return 0;
+//}
+
+//1*1=1
+//2*1=2 2*2=4
+//3*1=3 3*2=6 3*3=9
+//void print_table(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-3d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//	return 0;
+//}
+
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+//void reverse_string(char arr[])
+//{
+//	int left = 0;
+//	int right = my_strlen(arr) - 1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+
+//void reverse_string(char arr[])
+//{
+//	char tmp = arr[0];
+//	int len = my_strlen(arr);
+//	arr[0] = arr[len - 1];
+//	arr[len - 1] = '\0';
+//	if (my_strlen(arr + 1) >= 2)
+//		reverse_string(arr + 1);
+//	arr[len - 1] = tmp;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdefg";//fedcba
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+//输入：1729，输出：19
+//Disgitsum(1729)
+//Disgitsum(172) + 1729%10
+//Disgitsum(17) + 172%10 +1729%10
+//Disgitsum(1) +17%10 + ...
+//1+7+2+9
+//int Disgitsum(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		return Disgitsum(num / 10) + num % 10;
+//	}
+//	else
+//	{
+//		return num;
+//	}
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);//1729
+//	int ret = Disgitsum(num);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+//double Pow(int n, int k)
+//{
+//	//n^k = n * n^(k-1)
+//	if (k < 0)
+//		return (1.0 / (Pow(n, -k)));
+//	else if (k == 0)
+//		return 1;
+//	else
+//		return n * Pow(n, k - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d%d", &n, &k);
+//	double ret = Pow(n, k);
+//	printf("ret = %lf\n", ret);
+//	return 0;
+//}
+
+//描述一个学生
+//名字
+//年龄
+//电话
+//性别
+
+//struct 结构体关键字  Stu - 结构体标签  struct Stu - 结构体类型
+//struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}s1, s2, s3;//s1,s2,s3是三个全局的结构体变量
+
+//typedef struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+//
+//int main()
+//{
+//	struct Stu s1 = { "张三",20,"15249287079","男" };//局部变量
+//	Stu s2 = { "旺财",30,"15596668862","保密" };
+//	return 0;
+//}
+
+//struct S
+//{
+//	int a;
+//	char c;
+//	char arr[20];
+//	double d;
+//};
+//
+//struct T
+//{
+//	char ch[10];
+//	struct S s;
+//	char* pc;
+//};
+//
+//int main()
+//{
+//	char arr[] = "hello bit\n";
+//	struct T t = { "hehe",{100,'w',"hello world",3.14},arr };
+//	printf("%s\n", t.ch);//hehe
+//	printf("%s\n", t.s.arr);//hello world
+//	printf("%lf\n", t.s.d);//3.14
+//	printf("%s\n", t.pc);//hello bit
+//	return 0;
+//}
+
+//typedef struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+//
+//void Print1(Stu tmp)
+//{
+//	printf("name: %s\n", tmp.name);
+//	printf("age:  %d\n", tmp.age);
+//	printf("tele: %s\n", tmp.tele);
+//	printf("sex:  %s\n", tmp.sex);
+//}
+//
+//void Print2(Stu* ps)
+//{
+//	printf("name: %s\n", ps->name);
+//	printf("age:  %d\n", ps->age);
+//	printf("tele: %s\n", ps->tele);
+//	printf("sex:  %s\n", ps->sex);
+//}
+//
+//int main()
+//{
+//	Stu s = {"李四",40,"15598886688","男"};
+//	//打印结构体数据
+//	//Print1 和 Print2 哪个更好
+//	Print1(s);
+//	Print2(&s);
+//	return 0;
+//}
+
+int Add(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int ret = 0;
+	ret = Add(a, b);
+	return 0;
+}
